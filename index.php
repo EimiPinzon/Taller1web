77388d25b2e14bf7eed6a2da2f2370b4e43e6a27
EimiPinzon <eimipinzonhoyos@gmail.com>
2020-06-13 20:45:30 -0500

    Realizaciòn del primer taller de Web1

---
+++ index.php
@@ -0,0 +1,160 @@
+<?php
+// 1. Programa que sirve de calculadora arítmetica
+//Programado por Eimi Pinzón Hoyos GRUPO#1
+
+/*$numero1=5;
+$numero2=10;
+
+$suma=$numero1+$numero2;
+$resta=$numero1-$numero2;
+$multiplicacion=$numero1*$numero2;
+$division=$numero1/$numero2;
+
+
+echo("------------------------------------");
+echo("<br>");
+echo("Calculadora 3000");
+echo("<br>");
+echo("La mejor del mundo");
+echo("<br>");
+echo("------------------------------------");
+echo("<br>");
+echo("<br>");
+
+echo("La suma de: ".$numero1."+".$numero2."=".$suma);
+echo("<br>");
+echo("La resta de: ".$numero1."-".$numero2."=".$resta);
+echo("<br>");
+echo("La multiplicación de: ".$numero1."+".$numero2."=".$multiplicacion);
+echo("<br>");
+echo("La división de: ".$numero1."/".$numero2."=".$division);*/
+
+
+//**************************************//
+
+/*echo("----------------------------------------------");
+echo("<br>");
+echo("<br>");
+
+// 2. Código para calcular el indice de masa corporal
+
+$peso=58;
+$altura1=1.73;
+$altura2=1.73;
+
+$IMC1=$altura1*$altura2;
+$IMC2=$peso/$IMC1;
+
+
+echo("GIMNASIO BODYTECH");
+echo("<br>");
+echo("Calcule su indice de masa corporal IMC");
+
+echo("<br>");
+echo("<br>");
+echo("----------------------------------------------");
+echo("<br>");
+echo("<br>");
+
+
+echo("Señor usuario, su indice de masa corporal es: ".$IMC2);
+echo("<br>");
+if($IMC2 <= 18.4){
+    echo("y usted tiene insuficiencia de peso, por favor cuida de ti mismo y pon más cuidado en tu alimentación.");
+}elseif($IMC2 >= 18.5 && $IMC2 <= 24.9){
+    echo("Y usted tiene un peso normal, sigue así de bien.");
+}elseif($IMC2 >= 25 && $IMC2 <= 29.9){
+    echo("Y usted tiene sobrepeso, por favor intente llevar una vida más saludable.");
+}elseif($IMC2 >= 30 && $IMC2 <= 34.9){
+    echo("Y usted tiene obesidad 1, pero te tenemos la solución, si trabajas constantemente en nuestro gimnasio podras mejorar tu salud.");
+}elseif($IMC2 >= 35 && $IMC2 <= 39.9){
+    echo("Y usted tiene obesidad 2, por favor no hagas a un lado tu salud, en nuestros gimnasios puedes trabajar para mejorar cada día.");
+}elseif($IMC2 >= 40){
+    echo("Y usted tiene obesidad 3, por favor no dejes de visitar nuestro gimnasio, pronto lograrás tu peso ideal.");
+}else{
+    echo("Lo invitamos a que pida una cita con nuestra nutricionista, para así recomendarle una alimentación más saludable y un mejor estilo de vida. la salud es primero.");
+}*/
+
+
+//******************************//
+
+// 3. Promoción Spring Step
+
+/*$cantidadZapatos=2;
+$costoneto=150000;
+$totalCompra=($cantidadZapatos * $costoneto);
+$valordescuento1= $totalCompra * 0.10;
+$valordescuento2= $totalCompra * 0.20;
+$valordescuento3= $totalCompra * 0.40;
+$valordescuento=0;
+$valorPagar=($cantidadZapatos * $costoneto);
+
+if ($cantidadZapatos == 3){
+    echo("Señor usuario, el valor de su descuento es de: ". $valordescuento1);
+    echo(" y su vaalor a apgar es: ".$valorPagar=($cantidadZapatos * $costoneto)-$valordescuento1);
+} else if ($cantidadZapatos > 3 && $cantidadZapatos <= 5){
+    echo("Señor usuario, el valor de su descuento es de: ". $valordescuento2);
+    echo(" y su valor a apagar es: ".$valorPagar=($cantidadZapatos * $costoneto)-$valordescuento2);
+} else if ($cantidadZapatos > 5){
+    echo("Señor usuario, el valor de su descuento es de: ". $valordescuento3);
+    echo(" y su valor a apagar es: ".$valorPagar=($cantidadZapatos * $costoneto)-$valordescuento3);
+}else{
+    echo("Cantidad Vendida: ". $cantidadZapatos);
+    echo("<br>");
+    echo("Coste neto: ".$totalCompra);
+    echo("<br>");
+    echo("Valor  del descuento: ".$valordescuento);
+    echo("<br>");
+    echo("Valor a pagar: ".$valorPagar);
+}*/
+
+//***************************************//
+
+// 4. Sueldo semanal de trabajador Postobón
+
+/*echo("-----------------------------");
+echo("<br>");
+echo("Calcula tu sueldo POSTOBÓN");
+echo("<br>");
+echo("-----------------------------");
+echo("<br>");
+echo("<br>");
+
+$horas=48;
+$valhora=20000;
+$sueldon=$horas*$valhora;
+$sueldoex=(25000*$horas)+$sueldon-800000;
+
+if ($horas <= 40){
+    echo("Señor empleado, su sueldo es de: "."$".$sueldon);
+}else{
+    echo("Señor empleado, su sueldo con horas extras es de: "."$".$sueldoex);
+}*/
+
+
+//***************************************
+
+// 5. Consulta base de datos la jugosa
+
+
+/*echo("<br>");
+echo("-----------------------------");
+echo("<br>");
+echo("Base de datos La jugosa");
+echo("<br>");
+echo("-----------------------------");
+
+$salpicon=array("Dulce1"=>"Banano","Dulce2"=>"Manzana","Dulce3"=>"Durazno","Acido1"=>"Piña","Acido2"=>"Naranja","Acido3"=>"Lulo");
+
+echo("<br>");
+echo("<br>");
+
+foreach($salpicon as $clave=>$valor){
+    echo($clave.":".$valor);
+    echo("<br>");
+}*/
+
+
+
+?>
+
